e8478e77158d32165d73b9bc0d31e352cbd3ec61
LordMartron94 <m.david.vanderhoorn@gmail.com>
2021-07-14 18:30:53 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='JSON - Handling',
+    version='1.0.0',
+    packages=['json_storage_handling'],
+    url='https://pypi.python.org/pypi',
+    license='',
+    author='Matthew David van der Hoorn',
+    author_email='m.david.vanderhoorn@gmail.com',
+    description='To handle Json files. Nothing fancy really.'
+)
