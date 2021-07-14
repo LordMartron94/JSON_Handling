418cf699cb84125b41f0a1f3a61be686ddf48ee9
LordMartron94 <m.david.vanderhoorn@gmail.com>
2021-07-14 21:39:38 +0200

    Version 1.1.1

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='JSON - Handling',
-    version='1.0.0',
+    name='Json-Handling',
+    version='1.1.1',
     packages=['json_storage_handling'],
-    url='https://pypi.python.org/pypi',
+    url='https://github.com/LordMartron94/JSON_Handling/tree/master/json_storage_handling',
     license='',
     author='Matthew David van der Hoorn',
     author_email='m.david.vanderhoorn@gmail.com',
-    description='To handle Json files. Nothing fancy really.'
+    description='Nothing fancy really, basic Json handler.'
 )
